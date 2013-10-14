6dbd1e28802efc03663ff8a26ef27ab55de23fec
chenxuhao <cxh.illinois.edu@localhost>
2013-10-14 12:17:17 -0500

    lavaMD & NeuralNetwork added, config removed

--- scan/common.h
+++ scan/common.h
@@ -13,4 +13,24 @@
 //#define FORWARD_ATOMIC
 //#define HW_BARRIER
 //#define NAIVE
+
+#define CUDA_SAFE_CALL_NO_SYNC(call) {                                       \
+    cudaError err = call;                                                    \
+    if( cudaSuccess != err) {                                                \
+        fprintf(stderr, "Cuda error in file '%s' in line %i : %s.\n",        \
+                __FILE__, __LINE__, cudaGetErrorString( err) );              \
+        exit(EXIT_FAILURE);                                                  \
+    } }
+
+#define CUDA_SAFE_CALL(call) CUDA_SAFE_CALL_NO_SYNC(call);
+
+#define CUT_CHECK_ERROR(errorMessage) {                                      \
+    cudaError_t err = cudaGetLastError();                                    \
+    if( cudaSuccess != err) {                                                \
+        fprintf(stderr, "Cuda error: %s in file '%s' in line %i : %s.\n",    \
+                errorMessage, __FILE__, __LINE__, cudaGetErrorString( err) );\
+        exit(EXIT_FAILURE);                                                  \
+    }                                                                        \
+  }
+
 #endif
