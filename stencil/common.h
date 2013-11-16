0af73b7ad117b5e49dde21ccc0dd3989b7cf599d
chenxuhao <cxh.illinois.edu@localhost>
2013-11-16 16:14:18 -0600

    stencil updated

--- stencil/common.h
+++ stencil/common.h
@@ -1,42 +1,12 @@
-#ifndef COMMON_H
-#define COMMON_H
+/***************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ***************************************************************************/
 
+#ifndef _COMMON_H_
+#define _COMMON_H_
 #define Index3D(_nx,_ny,_i,_j,_k) ((_i)+_nx*((_j)+_ny*(_k)))
-#define PI 3.1415926
-//#define tx threadIdx.x
-//#define bx blockIdx.x
-//#define bs blockDim.x
-#define BSX 32
-#define BSY 4
-#define BSZ 4
-#define ATOMIC
-//#define LOCKFREE
-//#define HW_BARRIER
-
-//#define TIMING
-//#define NAIVE
-//#define TILE_2D
-//#define TILE_2D_OLD
-#define TILE_3D
-#define TILE_3D_NEW
-
-#define CUDA_SAFE_CALL_NO_SYNC(call) {                                       \
-    cudaError err = call;                                                    \
-    if( cudaSuccess != err) {                                                \
-        fprintf(stderr, "Cuda error in file '%s' in line %i : %s.\n",        \
-                __FILE__, __LINE__, cudaGetErrorString( err) );              \
-        exit(EXIT_FAILURE);                                                  \
-    } }
-
-#define CUDA_SAFE_CALL(call) CUDA_SAFE_CALL_NO_SYNC(call);
-
-#define CUT_CHECK_ERROR(errorMessage) {                                      \
-    cudaError_t err = cudaGetLastError();                                    \
-    if( cudaSuccess != err) {                                                \
-        fprintf(stderr, "Cuda error: %s in file '%s' in line %i : %s.\n",    \
-                errorMessage, __FILE__, __LINE__, cudaGetErrorString( err) );\
-        exit(EXIT_FAILURE);                                                  \
-    }                                                                        \
-  }
-
 #endif
